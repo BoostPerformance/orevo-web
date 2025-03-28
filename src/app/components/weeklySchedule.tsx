8eab5f8cf855cf24ab8e7eed7382bd51de85f7e7
sung-eunji <danceunji@gmail.com>
2025-03-28 12:58:22 +0100

    xs media query at Weekly Schedule

--- src/app/components/weeklySchedule.tsx
+++ src/app/components/weeklySchedule.tsx
@@ -1,17 +1,7 @@
-// interface ScheduleData {
-//   time: string;
-// }
-
 type DayType = '월' | '화' | '수' | '목' | '금';
 type ScheduleType = Record<DayType, string>;
 
 export default function WeeklySchedule() {
-  // const times: ScheduleData[] = [
-  //   { time: '오전 08:15' },
-  //   { time: '오전 09:30' },
-  //   { time: '오전 10:45' },
-  // ];
-
   const days: DayType[] = ['월', '화', '수', '목', '금'];
   const schedule: ScheduleType = {
     월: '근력',
@@ -22,16 +12,16 @@ export default function WeeklySchedule() {
   };
 
   return (
-    <div className="w-full bg-[#F8F7F4] rounded-lg p-6 border-green-800">
+    <div className="w-full bg-[#F8F7F4] rounded-lg p-6 xs:p-2 border-green-800 text-gray-13">
       <div className="overflow-x-auto">
-        <table className="w-full border-collapse">
+        <table className="w-full border-collapse text-sm xs:text-1-500">
           <thead>
             <tr>
-              <th className="p-4 text-center min-w-[120px] border-b border-t-2 border-r border-green-800"></th>
+              <th className="p-4 xs:p-2 text-center min-w-[80px] xs:min-w-[60px] border-b border-t-2 border-r border-green-900 xs:text-gray-5"></th>
               {days.map((day) => (
                 <th
                   key={day}
-                  className={`p-4 text-center min-w-[100px] font-medium border-t-2 border-b border-green-800 ${
+                  className={`p-4 xs:p-2 text-center min-w-[60px] xs:min-w-[29px] font-medium border-t-2 border-b border-green-800  ${
                     day === '화' || day === '목' ? 'bg-white' : 'bg-[#F8F7F4]'
                   }`}
                 >
@@ -42,14 +32,15 @@ export default function WeeklySchedule() {
           </thead>
           <tbody>
             <tr className="border-b border-green-800">
-              <td className="p-4 text-center font-medium border-t border-l border-r border-green-800">
-                오전 08:15
+              <td className="p-4 xs:p-1 text-center font-medium border-t border-l border-r border-green-800">
+                <span className="xs:text-0.875-500">08:15</span>
+                <span className="xs:hidden">오전 08:15</span>
               </td>
               {days.map((day) => (
                 <td
                   key={day}
                   rowSpan={3}
-                  className={`p-4 text-center border-r border-green-800 ${
+                  className={`p-4 xs:p-0 text-center border-r border-green-800 xs:text-gray-5 ${
                     day === '화' || day === '목' ? 'bg-white' : 'bg-[#F8F7F4]'
                   }`}
                 >
@@ -58,19 +49,21 @@ export default function WeeklySchedule() {
               ))}
             </tr>
             <tr className="border-b border-green-800">
-              <td className="p-4 text-center font-medium border-l border-r border-green-800">
-                오전 09:30
+              <td className="p-4 xs:p-2 text-center font-medium border-l border-r border-green-800">
+                <span className="xs:text-0.875-500">09:30</span>
+                <span className="xs:hidden">오전 09:30</span>
               </td>
             </tr>
             <tr className="border-b border-green-800 last:border-b-2">
-              <td className="p-4 text-center font-medium border-l border-r border-green-800">
-                오전 10:45
+              <td className="p-4 xs:p-2 text-center font-medium border-l border-r border-green-800">
+                <span className="xs:text-0.875-500">10:45</span>
+                <span className="xs:hidden">오전 10:45</span>
               </td>
             </tr>
           </tbody>
         </table>
       </div>
-      <p className="mt-4 text-sm text-gray-600 text-center">
+      <p className="mt-4 text-sm xs:text-0.75-500 text-gray-600 text-center">
         *스튜디오 사정으로 인하여 스케줄이 변경될 수도 있습니다.
       </p>
     </div>
