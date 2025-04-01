c80f4f0a82d87e4209b5c9a3bdd94557b9fe407d
Heeju Jo <cuteprobe@gmail.com>
2025-04-01 17:50:03 +0900

    Changed loading img from logo to green circle. Adjusted payment success page

--- src/app/components/common/loading.tsx
+++ src/app/components/common/loading.tsx
@@ -1,17 +1,10 @@
-import Image from 'next/image';
 interface LoadingProps {
   ismessage?: boolean;
 }
 export default function Loading({ ismessage }: LoadingProps) {
   return (
     <div className="flex flex-col justify-center items-center h-screen">
-      <Image
-        src="/images/logo-2.png"
-        alt="로딩중 로고"
-        width={100}
-        height={100}
-        className="animate-spin"
-      />
+      <div className="w-[100px] h-[100px] border-[8px] border-t-[#005E38] border-r-[#005E38] border-b-[#005E38] border-l-transparent rounded-full animate-spin"></div>
       {ismessage && (
         <div className="py-[2rem] text-1.5-400">결제가 진행중입니다.</div>
       )}
--- src/app/payment-success/page.tsx
+++ src/app/payment-success/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 export default function PaymentSuccess() {
   return (
-    <div className="flex py-[8rem] justify-center relative sm:items-center sm:flex-col sm:py-[6rem] sm:left-0">
+    <div className="w-full max-w-4xl mx-auto flex py-[8rem] justify-center relative sm:items-center sm:flex-col sm:py-[6rem] sm:left-0">
       {/* <Image
         src="/images/sneakers.png"
         width={100}
@@ -14,7 +14,7 @@ export default function PaymentSuccess() {
         "
         priority
       /> */}
-      <div className="flex flex-col gap-[3rem] w-[40rem] sm:w-auto sm:px-[3rem]">
+      <div className="flex flex-col gap-[3rem] w-[40rem] sm:w-auto sm:px-[2rem]">
         {/* <Image
           src="/images/logo.png"
           width={200}
@@ -22,20 +22,17 @@ export default function PaymentSuccess() {
           alt="logo"
           className="sm:hidden"
         /> */}
-        <div className="flex flex-col sm:items-center sm:justify-center ">
-          <h1 className="text-1.875-300 font-theJamsil font-bold">
+        <div className="flex flex-col sm:items-center sm:justify-center">
+          <h1 className="text-1.875-300 font-theJamsil font-bold text-gray-5 xs:px-12">
             신청이 완료되었습니다.
           </h1>
-          <p className="text-1.5-400 sm:text-center sm:text-base">
-            <br />
-            곧 작성하신 연락처로
-            <br />
-            연락드릴게요!
+          <p className="text-1.5-400 sm:text-center sm:text-base text-gray-5 mt-4 xs:text-1.25-500 xs:px-12">
+            곧 작성하신 연락처로 연락드릴게요!
           </p>
         </div>
 
-        <Link href="./">
-          <Button variant="green">홈으로가기</Button>
+        <Link href="./" className="self-center">
+          <Button variant="green">홈으로</Button>
         </Link>
       </div>
       <div className="absolute z-0 right-[20rem] sm:right-0 sm:hidden md:relative md:right-[20rem] md:z-[-1]">
